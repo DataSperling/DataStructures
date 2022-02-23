f6e3bffa470f25d3506eff8fedc373dc51101109
DataSperling <simon.francis.mrsc@tutanota.de>
2022-02-23 15:47:01 +0100

    added linked list node class

--- LinkedList/Java/LinkedList.java
+++ LinkedList/Java/LinkedList.java
@@ -1,1 +1,5 @@
-public class LinkedList {}
+public class LinkedList {
+
+    
+    
+ }
---
+++ LinkedList/Java/Node.java
@@ -0,0 +1,36 @@
+
+public class Node<T> {
+
+    private T data;
+    private Node<T> next;
+    
+    /*
+    * Constructs a new node with given data AND nex reference
+    *
+    * @param data: the data to be stored in the node
+    * @param next: reference to next node in list
+    */
+    Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+    
+    /*
+    * Node constructor with only data field for adding to list
+    *
+    * @param data: the data to be added to the node
+    */    
+    Node(T data) {
+        this(data, nll)
+    }
+    
+    /*
+    * Getter for data
+    *
+    * @return: the data to return
+    */
+    
+
+
+
+}
