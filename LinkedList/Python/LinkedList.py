b63c55efa1f51ad7e5b89fff9284cddf5f64e373
DataSperling <simon.francis.mrsc@tutanota.de>
2022-03-11 14:52:10 +0100

    added Python LinkedList classes

---
+++ LinkedList/Python/LinkedList.py
@@ -0,0 +1,14 @@
+class LinkedList:
+
+    # constructor
+    def __init__ (self):
+        self.head_node = None
+        
+        
+    # add_to_front
+    def AddToFront(self, newdata):
+        new_node = Node(data)
+        new_node.ref = self.head_node
+        self.head_node
+        
+    print("exit with code 0")
